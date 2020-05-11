dca8dbbcfd39d8af049eea76f1ae02437cd7be8e
Rafael Souza <f796751@mail.caixa>
2020-05-10 23:14:21 -0300

    teste

--- src/main/java/com/homecare/service/LoginService.java
+++ src/main/java/com/homecare/service/LoginService.java
@@ -1,21 +1,34 @@
 package com.homecare.service;
 
-import com.homecare.service.auth.AuthService;
+//import com.homecare.service.auth.AuthService;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import com.homecare.model.entity.Usuario;
 import com.homecare.repository.LoginRepository;
+import com.homecare.utils.exceptions.custom.ErrorResouceException;
 
-import javax.inject.Inject;
+import static com.homecare.utils.messages.ExceptionMessages.ERRO_TKN_INV;
 
 public class LoginService extends LoginRepository {
-    @Inject
-    private AuthService authService;
+//    @Inject
+//    private AuthService authService;
 
     public Usuario save(Usuario usuario) {
+
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCJ9.AbIJTDMFc7yUa5MhvcP03nJPyCPzZtQcGEp-zWfOkEE";
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("auth0")
+                    .build(); //Reusable verifier instance
+            DecodedJWT jwt = verifier.verify(token);
+        } catch (JWTVerificationException exception){
+            //Invalid signature/claims
+        }
         Usuario novoUsuario = super.save(usuario);
-
-        String token = this.authService.gerarToken();
-        this.authService.validarToken(token);
-
         return  novoUsuario;
     }
 
--- src/main/java/com/homecare/service/auth/AuthService.java
+++ src/main/java/com/homecare/service/auth/AuthService.java
@@ -1,54 +1,54 @@
-package com.homecare.service.auth;
-
-import com.homecare.utils.exceptions.custom.ErrorResouceException;
-import io.fusionauth.jwt.Signer;
-import io.fusionauth.jwt.Verifier;
-import io.fusionauth.jwt.domain.JWT;
-import io.fusionauth.jwt.hmac.HMACSigner;
-import io.fusionauth.jwt.hmac.HMACVerifier;
-
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-
-import static com.homecare.utils.messages.ExceptionMessages.ERRO_TKN_INV;
-
-public class AuthService {
-
-    public boolean validarToken(String encodedJWT){
-        boolean autenticado = false;
-
-        try {
-            // Build an HMC verifier using the same secret that was used to sign the JWT
-            Verifier verifier = HMACVerifier.newVerifier(System.getProperty("application.secret.app"));
-            // Verify and decode the encoded string JWT to a rich object
-            JWT jwt = JWT.getDecoder().decode(encodedJWT, verifier);
-            // Assert the subject of the JWT is as expected
-           autenticado = jwt.subject.equals(System.getProperty("application.secret.key"));
-        } catch (Exception exception){
-            throw new ErrorResouceException(ERRO_TKN_INV);
-        }
-
-        return autenticado;
-    }
-
-    public String gerarToken(){
-        String token = null;
-
-        try {
-            // Build an HMAC signer using a SHA-256 hash
-            Signer signer = HMACSigner.newSHA256Signer(System.getProperty("application.secret.app"));
-            // Build a new JWT with an issuer(iss), issued at(iat), subject(sub) and expiration(exp)
-            JWT jwt = new JWT().setIssuer("www.homecare.com")
-                    .setIssuedAt(ZonedDateTime.now(ZoneOffset.UTC))
-                    .setSubject(System.getProperty("application.secret.key"))
-                    .setExpiration(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(60));
-
-            // Sign and encode the JWT to a JSON string representation
-            token = JWT.getEncoder().encode(jwt, signer);
-        } catch (Exception exception){
-            throw new ErrorResouceException(ERRO_TKN_INV);
-        }
-
-        return token;
-    }
-}
+//package com.homecare.service.auth;
+//
+//import com.homecare.utils.exceptions.custom.ErrorResouceException;
+//import io.fusionauth.jwt.Signer;
+//import io.fusionauth.jwt.Verifier;
+//import io.fusionauth.jwt.domain.JWT;
+//import io.fusionauth.jwt.hmac.HMACSigner;
+//import io.fusionauth.jwt.hmac.HMACVerifier;
+//
+//import java.time.ZoneOffset;
+//import java.time.ZonedDateTime;
+//
+//import static com.homecare.utils.messages.ExceptionMessages.ERRO_TKN_INV;
+//
+//public class AuthService {
+//
+////    public boolean validarToken(String encodedJWT){
+////        boolean autenticado = false;
+////
+////        try {
+////            // Build an HMC verifier using the same secret that was used to sign the JWT
+////            Verifier verifier = HMACVerifier.newVerifier(System.getProperty("application.secret.app"));
+////            // Verify and decode the encoded string JWT to a rich object
+////            JWT jwt = JWT.getDecoder().decode(encodedJWT, verifier);
+////            // Assert the subject of the JWT is as expected
+////           autenticado = jwt.subject.equals(System.getProperty("application.secret.key"));
+////        } catch (Exception exception){
+////            throw new ErrorResouceException(ERRO_TKN_INV);
+////        }
+////
+////        return autenticado;
+////    }
+////
+////    public String gerarToken(){
+////        String token = null;
+////
+////        try {
+////            // Build an HMAC signer using a SHA-256 hash
+////            Signer signer = HMACSigner.newSHA256Signer(System.getProperty("application.secret.app"));
+////            // Build a new JWT with an issuer(iss), issued at(iat), subject(sub) and expiration(exp)
+////            JWT jwt = new JWT().setIssuer("www.homecare.com")
+////                    .setIssuedAt(ZonedDateTime.now(ZoneOffset.UTC))
+////                    .setSubject(System.getProperty("application.secret.key"))
+////                    .setExpiration(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(60));
+////
+////            // Sign and encode the JWT to a JSON string representation
+////            token = JWT.getEncoder().encode(jwt, signer);
+////        } catch (Exception exception){
+////            throw new ErrorResouceException(ERRO_TKN_INV);
+////        }
+////
+////        return token;
+////    }
+//}
