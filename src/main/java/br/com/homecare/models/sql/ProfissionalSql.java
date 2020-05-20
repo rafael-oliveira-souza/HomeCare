d66c2d378aa285d3daa01a08783a67b193ea9390
Rafael Souza <f796751@mail.caixa>
2020-05-19 21:53:27 -0300

    Configuracoes postgress

---
+++ src/main/java/br/com/homecare/models/sql/ProfissionalSql.java
@@ -0,0 +1,8 @@
+package br.com.homecare.models.sql;
+
+public class ProfissionalSql {
+	public static final String RELACAO_PROFISSIONAL_SINTOMAS = 
+			"SELECT FROM OBJ PROFISSIONAL OBJ" +
+			"INNER JOIN OBJ.";
+
+}
--- src/main/java/br/com/homecare/repositories/ProfissionalRepository.java
+++ src/main/java/br/com/homecare/repositories/ProfissionalRepository.java
@@ -1,11 +1,17 @@
 package br.com.homecare.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.homecare.models.entities.Profissional;
 
 @Repository
 public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
+	
+//	@Query("Select *FROM PROFISSIONAL WHERE") 
+//	public List<Profissional> buscarProfissionaisRelacionadosSintomas();
 
 }
